4a37f9a217534c15c6da47de3fabaa8fd1b5c23b
guouille-dev <64792762+guouille-dev@users.noreply.github.com>
2020-05-04 17:01:40 +0200

    Create none.py

---
+++ none.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+import pandas as pd
+
+df = pd.Serie(np.arange(1, 100, 0.5))
+
+print("df.levels = {}".format(df.levels))
+
+print("dataFrame : \n {}".format(df))
+
